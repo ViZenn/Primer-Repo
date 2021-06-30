6a5648a455201fd75858fd06d421b8c0c46690e3
Vicente <vaiturriaga@uc.cl>
2021-06-30 19:15:14 -0400

    este es el nuevo commit para actualizar el codigo

--- Primer archivo.py
+++ Primer archivo.py
@@ -1,3 +1,6 @@
 Primer archivo
 
 print('hola, este es el primer archivo hecho en git')
+
+print('ya hice un commit y lo subi a github')
+print('ahora estoy haciendo una actualizacion del codigo a ver que wea')
